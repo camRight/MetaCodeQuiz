26ed9e0b0db8aa5df5900623b009948ed19e3ba7
Cameron Wright <cam.mac.wright@gmail.com>
2020-10-29 22:46:47 -0700

    timer starting numbers added

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 window.onload = function () {
     
-    let timerMinutesPush = document.getElementById("minutesLeft")
-    let timerSecondsPush = document.getElementById("secondsLeft")
+
+    let countMin = 
+    let countSec = 
 
 
+    let timerMinutesPush = document.getElementById("minutesLeft").innerHTML = countMin
+    let timerSecondsPush = document.getElementById("secondsLeft").innerHTML = countSec
 
     // set to click event. check for 1 click with ms delay
     function timer () {
