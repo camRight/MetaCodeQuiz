55f9927e1d0aeffd39913b03c142f5bbee646aa5
Cameron Wright <cam.mac.wright@gmail.com>
2020-10-29 22:42:44 -0700

    domTraverseTimer

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 window.onload = function () {
     
+    let timerMinutesPush = document.getElementById("minutesLeft")
+    let timerSecondsPush = document.getElementById("secondsLeft")
+
+
+
     // set to click event. check for 1 click with ms delay
     function timer () {
         var sec = 1200
