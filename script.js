2147300823cc772e062fc6086bf03fc3fb0569cc
Cameron Wright <cam.mac.wright@gmail.com>
2020-10-29 22:34:27 -0700

    starting timer

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 window.onload = function () {
-
+    
+    // set to click event. check for 1 click with ms delay
+    function timer () {
+        var sec = 1200
+        var timer = setInterval ()
+    }
 };
