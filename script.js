011a86a7ab478ce6474422151e138d7554b374ef
Cameron Wright <cam.mac.wright@gmail.com>
2020-11-08 14:05:19 -0800

    working timer with event

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
-window.onload = function () {
-    
+
     // Timer
 
-    let timerSecondsPush = document.getElementById("secondsLeft").innerHTML = countSec
-
-    let countSec = 200
 
 
+    let timerCount = 200;
+    let timerEl = document.querySelector("#secondsLeft");
+    let start = document.querySelector("#start")
+
+let x = window.setInterval( function() {
+            timerCount--;
+            timerEl.textContent = timerCount
+
+            if (timerCount === 0) {
+                clearInterval(x)
+            }
+        }, 2000)
     // submit start  button
-    document.querySelector("#start").addEventListener("click", function () {
-        
-        
-        countSec--;
-
-        if (countSec === 0) {
-            clearinterval()
-        }
     
-    })
+    
+    start.addEventListener("click", x)
+        // var clearDiv = document.getElementById("clearStart")
 
+        // function Clear() {
+        //     document.body.clearDiv.parentNode.removeChild(clearDiv);
+        // }
+  
 
-    if finished (
+    // display = document.querySelector("secondsLeft")
+    // let timerSecondsPush = document.getElementById("secondsLeft").innerHTML = countSec
+    // if finished (
         //localStorage.setItem("initials", "")
-    )
-};
+    // )
 
-document.forms['']
+
